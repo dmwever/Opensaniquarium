ad89269ec2df36cc8160bb588a570a06e5873444
dmwever <56411717+dmwever@users.noreply.github.com>
2025-08-22 15:01:38 -0500

    - Add fish food falling

--- FishFood/fish_food.gd
+++ FishFood/fish_food.gd
@@ -1,15 +1,17 @@
-extends Node2D
+extends RigidBody2D
 
+@export var pellet: AnimatedSprite2D
+@export var food_type: String
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pellet.play(food_type)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
+func _physics_process(_delta):
 	pass
 
 
-func _on_hurtbox_area_entered(area):
+func _on_hurtbox_area_entered(_area):
 	queue_free()
